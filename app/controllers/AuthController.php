0a32ffc3add7b972602ac8f8738dd47c7e4e4316
Yoan Dusoleil <y.dusoleil@outlook.com>
2023-02-07 22:02:38 +0100

    Proto controller Authentification et controller DB

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -1,1 +1,23 @@
 <?php
+
+class AuthController
+{
+    /**
+     * AuthController constructor.
+     * @param $login
+     * User mail address used as login
+     * @param $password
+     * User hashed and salted password
+     */
+    public function login($login, $password): void
+    {
+        $dbc = new DBController();
+        if ($dbc->comparePassword($login, $password)) {
+            echo "Authentification réussie";
+        } else {
+            echo "Authentification échouée";
+        }
+    }
+}
+
+?>
