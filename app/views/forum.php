5ae049ce4e6cc8798adadc063c25ef56473e2a29
Maxime <98154358+Maxime-Cllt@users.noreply.github.com>
2023-02-25 10:22:26 +0100

    Amélioration de la partie style du site
    Suppression de dark_mode.css car inutile
    Changement du nom de light_mode.css en UI_Theme.css
    MAJ des themes dans les views
    Ajout d'une page de profil de l'utilisateur + style

--- app/views/forum.php
+++ app/views/forum.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <link id="link" rel="stylesheet" type="text/css" href="/css/light_mode.css"/>
+    <link id="link" rel="stylesheet" type="text/css" href="/css/UI_Theme.css"/>
     <link rel="icon" type="image/png" href="img/neptune_icon.png"/>
     <title>Forum</title>
 </head>
--- app/views/user_Profil_Page.php
+++ app/views/user_Profil_Page.php
@@ -21,114 +21,85 @@ $db = new DBManage();
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link rel="icon" type="image/png" href="/img/neptune_icon.png"/>
-    <link id="link" rel="stylesheet" type="text/css" href="/css/light_mode.css"/>
+    <link id="link" rel="stylesheet" type="text/css" href="/css/UI_Theme.css"/>
     <link id="link" rel="stylesheet" type="text/css" href="/css/user_Profil_Page.css"/>
     <title>Compte</title>
 </head>
 <body>
 <?php require 'navBar.php'; ?>
 
-<div class="page-content page-container" id="page-content">
-    <div class="padding">
-        <div class="row container d-flex justify-content-center">
-            <div class="col-xl-6 col-md-12">
-                <div class="card user-card-full">
-                    <div class="row m-l-0 m-r-0">
-                        <div class="col-sm-4 bg-c-lite-green user-profile">
-                            <div class="card-block text-center text-white">
-                                <div id="userPage_img_container">
-                                    <img id="userPage_imgUser" title="Votre photo de profil" src=<?php
-                                    if ($user->profilePicture == 'default.png' or $user->profilePicture == null or strlen($user->profilePicture) <= 0 or !file_exists($user->profilePicture)) {
-                                        echo "img/default_user.png";
-                                    } else {
-                                        echo $user->profilePicture;
-                                    } ?>  onclick="" class="img-radius"
-                                         alt="User-Profile-Image">
-                                </div>
-                                <h6 class="f-w-600" style="text-align: center"
-                                    title="Pseudo"><?php echo $user->pseudo; ?></h6>
-                                <!--                                <i class=" mdi mdi-square-edit-outline feather icon-edit m-t-10 f-16"></i>-->
-                                <!--                                <p title="Identifiant"> -->
-                                <?php //echo $user->id; ?><!--</p>-->
-
-                                <a href='/userPage' class="bouton_edit_profile" title="Modifier le compte">
-                                    <img src="/img/edit-button.png" title="Modifier le compte" class="img_edit_profile">
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-sm-8">
-                            <div class="card-block">
-                                <h2 class="m-b-20 p-b-5 b-b-default f-w-600">Informations</h2>
-                                <div class="row">
-                                    <div class="col-sm-6">
-                                        <p class="m-b-10 f-w-600">Identifiant</p>
-                                        <h6 class="text-muted f-w-400" title="Identifiant"><?php echo $user->id; ?></h6>
-                                    </div>
-                                    <div class="col-sm-6">
-                                        <p class="m-b-10 f-w-600">Nom</p>
-                                        <h6 class="text-muted f-w-400" title="Nom"><?php echo $user->lastName; ?></h6>
-                                    </div>
-                                    <div class="col-sm-6">
-                                        <p class="m-b-10 f-w-600">Prénom</p>
-                                        <h6 class="text-muted f-w-400"
-                                            title="Prénom"><?php echo $user->firstName; ?></h6>
-                                    </div>
-                                    <div class="col-sm-6">
-                                        <p class="m-b-10 f-w-600">Email</p>
-                                        <h6 class="text-muted f-w-400"
-                                            title="Email"><?php echo $db->getLoginFromId($user->id)['login'] ?></h6>
-                                    </div>
-                                    <div class="col-sm-6">
-                                        <p class="m-b-10 f-w-600">Date de Naissance</p>
-                                        <h6 class="text-muted f-w-400"
-                                            title="Date de Naissance"><?php echo $user->birthDate; ?></h6>
-                                    </div>
-                                    <div class="col-sm-6">
-                                        <p class="m-b-10 f-w-600">Age</p>
-                                        <h6 class="text-muted f-w-400" title="Age">
-                                            <?php
-                                            /**
-                                             * @details Fonction qui calcul l'âge de l'utilisateur et qui l'affiche
-                                             * @param $date_naissance
-                                             * @return int
-                                             * @throws Exception
-                                             */
-                                            function calculer_age($date_naissance)
-                                            {
-                                                $interval = date_diff(new DateTime($date_naissance), new DateTime());
-                                                return $interval->y;
-                                            }
-
-                                            try {
-                                                echo calculer_age($user->birthDate);
-                                            } catch (Exception $e) {
-                                                echo "Impossible de calculer la date de naissance";
-                                            } ?>
-
-                                        </h6>
-                                    </div>
-                                </div>
-                                <h2 class="m-b-20 m-t-40 p-b-5 b-b-default f-w-600">Activités</h2>
-                                <div class="row">
-                                    <div class="col-sm-6">
-                                        <p class="m-b-10 f-w-600">Récent</p>
-                                        <h6 class="text-muted f-w-400">C++</h6>
-                                    </div>
-                                    <div class="col-sm-6">
-                                        <p class="m-b-10 f-w-600">Dernière notes</p>
-                                        <h6 class="text-muted f-w-400">20/20</h6>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+<div class="div_padding">
+    <div class="card user-card-div">
+        <div class="div_card_user user-profile">
+            <div class="card-div ">
+                <div id="userPage_img_container">
+                    <img id="userPage_imgUser" title="Votre photo de profil" src=<?php
+                    if ($user->profilePicture == 'default.png' or $user->profilePicture == null or strlen($user->profilePicture) <= 0 or !file_exists($user->profilePicture)) {
+                        echo "img/default_user.png";
+                    } else {
+                        echo $user->profilePicture;
+                    } ?>  class="img-radius" alt="User-Profile-Image">
                 </div>
+                <h2 style="text-align: center; font-weight: 900;" title="Pseudo"><?php echo $user->pseudo; ?>
+                </h2>
+
+                <a href='/userPage' class="bouton_edit_profile" title="Modifier le compte">
+                    <img src="/img/edit-button.png" class="img_edit_profile">
+                </a>
             </div>
+        </div>
+
+        <div class="card-div">
+            <h2 class="titre_section">Informations</h2>
+
+            <p class="titre_element">Identifiant</p>
+            <h6 class="text_element" title="Identifiant"><?php echo $user->id; ?></h6>
+
+            <p class="titre_element">Nom</p>
+            <h6 class="text_element" title="Nom"><?php echo $user->lastName; ?></h6>
+
+            <p class="titre_element">Prénom</p>
+            <h6 class="text_element" title="Prénom"><?php echo $user->firstName; ?></h6>
+
+            <p class="titre_element">Email</p>
+            <h6 class="text_element" title="Email"><?php echo $db->getLoginFromId($user->id)['login'] ?></h6>
+
+            <p class="titre_element">Date de Naissance</p>
+            <h6 class="text_element" title="Date de Naissance"><?php echo $user->birthDate; ?></h6>
+
+            <p class="titre_element">Age</p>
+            <h6 class="text_element" title="Age">
+                <?php
+                /**
+                 * @details Fonction qui calcul l'âge de l'utilisateur et qui l'affiche
+                 * @param $date_naissance
+                 * @return int
+                 * @throws Exception
+                 */
+                function calculer_age($date_naissance)
+                {
+                    return date_diff(new DateTime($date_naissance), new DateTime())->y;
+                }
+
+                try {
+                    echo calculer_age($user->birthDate);
+                } catch (Exception $e) {
+                    echo "Impossible de calculer la date de naissance";
+                } ?>
+
+            </h6>
+
+            <h2 class="titre_section">Activités</h2>
+
+            <p class="titre_element">Récent</p>
+            <h6 class="text_element">C++</h6>
+
+            <p class="titre_element">Dernière notes</p>
+            <h6 class="text_element">20/20</h6>
+
         </div>
     </div>
 </div>
 
 <script src="/js/UI_Theme.js"></script>
-
 </body>
-
